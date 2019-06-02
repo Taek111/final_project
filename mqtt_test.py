f0a3a4a5d9abaca1e3f8b85aac33ff396c8012b0
root <rkxornjs@skku.edu>
2019-06-02 23:26:25 +0900

    beta ver

--- mqtt_test.py
+++ mqtt_test.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 import time
 import struct
 import HomeCare
-
+import threading
 username = "yellowdog"
 room_list = ["room1", "room2", "room3"] #id 1,2
 app = HomeCare.HomeCare(room_list, username)
@@ -13,27 +13,39 @@ def on_connect(client, userdata, rc):
     client.subscribe("connected")
 
 def on_message(client, userdata, msg):
-    topic = str(msg.topic.decode("utf-8"))
+    
+    topic = str(msg.topic)
+    print(topic)
     if topic == "cds":
         id,con = struct.unpack('<BB', msg.payload[0:2])
     elif topic == "pir":
         id,con = struct.unpack('<BB', msg.payload[0:2])
 
-    elif topic == "ir":
-            id = "outdoor"
-            con = struct.unpack('B', msg.payload)
+    elif topic == "isOpen":
+        print(topi)
+        id = "outdoor"
+        con = struct.unpack('h', msg.payload[0:1])[0]
+    elif topic == "help":
+        id = "helpcall"
+        con = struct.unpack('B', msg.payload)[0]
+        if not con:#voice "help"
+            if app.audio.get_busy():
+                app.audio.stop()
+            app.isEmergency = True
+            Eapp = threading.Thread(target=app.Emergency_one)
+            Eapp.start()
+        else: # voice "ok"
+            if app.audio.get_busy():
+                app.audio.stop()
+            app.isEmergency = False
+    elif topic == "temperature":
+        id = "temp_mini"
+        con = struct.unpack('B',msg.payload)[0]
+   # elif topic == "realcds":
+    #    id = "value"
+     #   con=struct.unpack('<Bh',msg.payload[0:3])
 
-        elif topic == "help":
-            id = "helpcall"
-            con = struct.unpack('B', msg.payload)[0]
-            if con:#voice "help"
-                if app.audio.get_busy():
-                    app.audio.stop()
-                app.EmergencyCall()
-            else: # voice "ok"
-                if app.audio.get_busy():
-                    app.audio.stop()
-                app.isEmergency = False
+
 
     print(topic, id, con)
     app.data_in(id, topic, con)
@@ -49,7 +61,9 @@ def main():
     client.subscribe("cds")
     client.subscribe("pir")
     client.subscribe("help")
-    client.subscribe("ir")
+    client.subscribe("isOpen")
+    client.subscribe("temperature")
+    client.subscribe("realcds")
     client.on_message = on_message		
     print("client connected")
     client.loop_forever()
